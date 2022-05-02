43f70d4bb9c5e45ddbf1239518051ff6113918ef
Roman Orlov <Orlov.R@citilink.ru>
2022-05-03 01:05:10 +0300

    second commit in feature2

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 }
 
 // this is first comment for feature2
+// this is second comment for feature2
 func StartServer(){
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
